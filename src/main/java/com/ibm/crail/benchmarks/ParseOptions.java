53d0db88fe874d86f47ae9f721f9dbc8b33661a6
Animesh Trivedi <atr@zurich.ibm.com>
2017-04-26 08:30:10 +0200

    Initial code check-in

---
+++ src/main/java/com/ibm/crail/benchmarks/ParseOptions.java
@@ -0,0 +1,54 @@
+/*
+ * Crail SQL Benchmarks
+ *
+ * Author: Animesh Trivedi <atr@zurich.ibm.com>
+ *
+ * Copyright (C) 2017, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.benchmarks;
+
+import org.apache.commons.cli.*;
+
+public class ParseOptions {
+    private Options options;
+
+    public ParseOptions(){
+        options = new Options();
+        options.addOption("h", "help", false, "show help.");
+    }
+
+    public void show_help() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("Main", options);
+    }
+    public void parse(String[] args) {
+        CommandLineParser parser = new GnuParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("h")) {
+                show_help();
+                System.exit(0);
+            }
+        } catch (ParseException e) {
+            System.err.println("Failed to parse command line properties" + e);
+            show_help();
+            System.exit(-1);
+        }
+    }
+}
---
+++ src/main/scala/com/ibm/crail/benchmarks/Main.scala
@@ -0,0 +1,35 @@
+/*
+ * Crail SQL Benchmarks
+ *
+ * Author: Animesh Trivedi <atr@zurich.ibm.com>
+ *
+ * Copyright (C) 2017, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.ibm.crail.benchmarks
+
+object Main {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+    val options = new ParseOptions()
+    options.parse(args)
+  }
+
+}
